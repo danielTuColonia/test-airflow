182dbdb661e83508ee554e5e461a8a918fc38ad9
danielTuColonia <134245656+danielTuColonia@users.noreply.github.com>
2024-08-14 12:47:33 -0500

    Update preprocess_data.py

--- dags/task/risk_profiling_dag/preprocess_data.py
+++ dags/task/risk_profiling_dag/preprocess_data.py
@@ -19,3 +19,4 @@ def preprocess_data(**kwargs):
     kwargs['ti'].xcom_push(key='preprocessed_data', value=data.to_dict())
 
     print("DG: Se ejecuto satisfactoriamente!")
+    print(data.to_dict())
