fa03cee8b2d5521359dc41895ec776d499d58ca9
danielTuColonia <134245656+danielTuColonia@users.noreply.github.com>
2024-08-14 12:31:39 -0500

    Update risk_profiling_dag.py

--- dags/risk_profiling_dag.py
+++ dags/risk_profiling_dag.py
@@ -26,48 +26,49 @@ install_dependencies = BashOperator(
     dag=dag,
 )
 
-# Importar las funciones desde los archivos .py en la carpeta task
-from task.risk_profiling_dag.preprocess_data import preprocess_data
-from task.risk_profiling_dag.train_model import train_model
-from task.risk_profiling_dag.evaluate_model import evaluate_model
-from task.risk_profiling_dag.hyperparameter_tuning import hyperparameter_tuning
-from task.risk_profiling_dag.generate_report import generate_report
+# # Importar las funciones desde los archivos .py en la carpeta task
+# from task.risk_profiling_dag.preprocess_data import preprocess_data
+# from task.risk_profiling_dag.train_model import train_model
+# from task.risk_profiling_dag.evaluate_model import evaluate_model
+# from task.risk_profiling_dag.hyperparameter_tuning import hyperparameter_tuning
+# from task.risk_profiling_dag.generate_report import generate_report
 
-# Definir las tareas del DAG utilizando los operadores de Python y las funciones importadas
-preprocess_task = PythonOperator(
-    task_id='preprocess_data',
-    python_callable=preprocess_data,
-    provide_context=True,
-    dag=dag,
-)
+# # Definir las tareas del DAG utilizando los operadores de Python y las funciones importadas
+# preprocess_task = PythonOperator(
+#     task_id='preprocess_data',
+#     python_callable=preprocess_data,
+#     provide_context=True,
+#     dag=dag,
+# )
 
-tune_task = PythonOperator(
-    task_id='hyperparameter_tuning',
-    python_callable=hyperparameter_tuning,
-    provide_context=True,
-    dag=dag,
-)
+# tune_task = PythonOperator(
+#     task_id='hyperparameter_tuning',
+#     python_callable=hyperparameter_tuning,
+#     provide_context=True,
+#     dag=dag,
+# )
 
-train_task = PythonOperator(
-    task_id='train_model',
-    python_callable=train_model,
-    provide_context=True,
-    dag=dag,
-)
+# train_task = PythonOperator(
+#     task_id='train_model',
+#     python_callable=train_model,
+#     provide_context=True,
+#     dag=dag,
+# )
 
-evaluate_task = PythonOperator(
-    task_id='evaluate_model',
-    python_callable=evaluate_model,
-    provide_context=True,
-    dag=dag,
-)
+# evaluate_task = PythonOperator(
+#     task_id='evaluate_model',
+#     python_callable=evaluate_model,
+#     provide_context=True,
+#     dag=dag,
+# )
 
-report_task = PythonOperator(
-    task_id='generate_report',
-    python_callable=generate_report,
-    provide_context=True,
-    dag=dag,
-)
+# report_task = PythonOperator(
+#     task_id='generate_report',
+#     python_callable=generate_report,
+#     provide_context=True,
+#     dag=dag,
+# )
 
 # Establecer la secuencia de tareas
-install_dependencies >> preprocess_task >> tune_task >> train_task >> evaluate_task >> report_task
+install_dependencies 
+# >> preprocess_task >> tune_task >> train_task >> evaluate_task >> report_task
