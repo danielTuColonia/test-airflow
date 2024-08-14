77b342ac5764ec04067f9018b1be9a73e751acde
danielTuColonia <134245656+danielTuColonia@users.noreply.github.com>
2024-08-14 13:14:12 -0500

    Update hyperparameter_tuning.py

--- dags/task/risk_profiling_dag/hyperparameter_tuning.py
+++ dags/task/risk_profiling_dag/hyperparameter_tuning.py
@@ -16,19 +16,26 @@ def hyperparameter_tuning(**kwargs):
     
     # Definir los parámetros para GridSearch
     param_grid = {
-        'n_estimators': [100, 200, 300],
-        'max_depth': [None, 10, 20, 30],
-        'min_samples_split': [2, 5, 10],
-        'min_samples_leaf': [1, 2, 4]
+        'n_estimators': [100, 200],
+        'max_depth': [ 10, 20],
+        'min_samples_split': [2],
+        'min_samples_leaf': [2, 4]
     }
     
     # Configurar el modelo y GridSearchCV
     model = RandomForestClassifier(random_state=42)
     grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
     
+    # Imprimir mensaje antes de iniciar el ajuste de hiperparámetros
+    print("Iniciando ajuste de hiperparámetros con GridSearchCV...")
+
     # Entrenar el modelo con búsqueda de hiperparámetros
     grid_search.fit(X_train, y_train)
     
+    # Imprimir mensaje al finalizar el ajuste de hiperparámetros
+    print("Ajuste de hiperparámetros completado.")
+    print(f"Mejores parámetros encontrados: {grid_search.best_params_}")
+
     # Guardar el mejor modelo encontrado
     best_model = grid_search.best_estimator_
     kwargs['ti'].xcom_push(key='best_model', value=best_model)
