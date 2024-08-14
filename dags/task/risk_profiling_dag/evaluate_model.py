b78e610f8065437d3c3f5c0d1b4ea8e280348f4f
danielTuColonia <134245656+danielTuColonia@users.noreply.github.com>
2024-08-14 13:21:22 -0500

    Update evaluate_model.py

--- dags/task/risk_profiling_dag/evaluate_model.py
+++ dags/task/risk_profiling_dag/evaluate_model.py
@@ -1,12 +1,18 @@
 import pandas as pd
+import pickle
 from sklearn.metrics import classification_report
 
 def evaluate_model(**kwargs):
-    # Cargar el modelo y los datos de prueba
-    model = kwargs['ti'].xcom_pull(key='model')
+    # Cargar la ruta del modelo desde XCom
+    model_path = kwargs['ti'].xcom_pull(key='best_model_path')
+    
+    # Deserializar el modelo desde el archivo pickle
+    with open(model_path, 'rb') as model_file:
+        model = pickle.load(model_file)
+    
+    # Cargar los datos de prueba desde XCom
     X_test_dict = kwargs['ti'].xcom_pull(key='X_test')
     y_test = kwargs['ti'].xcom_pull(key='y_test')
-    
     X_test = pd.DataFrame.from_dict(X_test_dict)
     
     # Hacer predicciones
@@ -15,6 +21,3 @@ def evaluate_model(**kwargs):
     # Evaluar el modelo
     report = classification_report(y_test, y_pred)
     print("Classification Report:\n", report)
-    
-    # Guardar el reporte para la siguiente tarea
-    kwargs['ti'].xcom_push(key='classification_report', value=report)
